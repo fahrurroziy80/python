50bc93123a4567f7e928fc56173903afa491149b
Ry <148849905+fahrurroziy80@users.noreply.github.com>
2025-05-27 10:33:11 +0700

    Add files via upload

---
+++ clas2.py
@@ -0,0 +1,32 @@
+a = 11 
+b = 2
+c = a % b
+print("ini adalah hasil dari a % b adalah =",c)#sisa bagi 
+
+a = 11 
+b = 2
+c = a + b
+print("ini adalah hasil dari a + b adalah = ",c)#penjumlahana = 
+b = 2
+c = a *  b
+print(f"Hasil dari a perkalian  b adalah = {c}")#perkalian
+
+a = 11 
+b = 2
+c = a - b
+print("ini adalah hasil dari a - b adalah = ",c)#pengurangan
+
+a = 10 
+b = 2
+c = a / b
+print("ini adalah hasil dari a / b adalah = ",c)#pembagian
+
+a = 8 
+b = 2
+c = a ** b
+print("ini adalah hasil dari a ** b adalah = ",c)#pangkat **
+
+a = 11 
+b = 2
+c = a // b
+print("ini adalah hasil dari a // b adalah = ",c)#pembagian
